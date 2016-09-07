b3dbbab87e8645c70fe79cbc1d94315883a3f465
marie-mccarthy <marie.mccarthy2000@gmail.com>
2016-09-07 08:03:36 -0600

    Not finished with all excersizes

--- JavaAPI/src/JavaAPI.java
+++ JavaAPI/src/JavaAPI.java
@@ -17,14 +17,15 @@ public class JavaAPI
 			//int lastLetter = word.length();
 			//int firstLetter= word.length()-1;
 			int lastLetter = word.length()-1;
-		//	System.out.println(word.length());
-			if(word.substring(lastLetter)==("s"))
+		;
+		//	String last = word.substring(lastLetter,word.length());
+			if( word.substring(lastLetter,word.length())==("s"))
 				{
-					System.out.println("plural");
+					System.out.println("not plural");
 				}
 			else
 				{
-					System.out.println(" not plural");
+					System.out.println("plural");
 				}
 			return word;
 		}
@@ -33,30 +34,54 @@ public class JavaAPI
 			System.out.println("input a word");
 			Scanner userInputWord2= new Scanner(System.in);
 			String word2 = userInputWord2.nextLine();			
-			String letters = word2.substring(0, word2.length());	
-			for(int i = 0; i<word2.length();i++)
-				{
-			switch(letters)
+			String letters = word2.substring(0, word2.length());
+			
+			for(int i = 1; i<word2.length();i++)
 			{
-				case"a":						
-				case"e":						
-				case"i":					
-				case"o":					
-				case"u":
-						{
-							vowel++;
-						}
-			}
+				if((letters == "a")||(letters == "e")||(letters == "i")||(letters == "o")||(letters == "u"))
+				{
+					vowel++;
 				}
-			System.out.println(vowel);
+					
+			}			
+			System.out.println(vowel+" vowel");
 			return word2;			
 		}
-		//public static String exercise3()
+		public static String exercise3()
 			{
-				System.out.println("input your name");
-				Scanner userInputWord3= new Scanner(System.in);
-				String name = userInputWord3.nextLine();	
-		//		return name;
+			System.out.println("input your full name");
+			Scanner userInputWord2= new Scanner(System.in);
+			String name = userInputWord2.nextLine();			
+			String letters2 = name.substring(0, name.length());
+			
+			for(int i = 1; i<name.length();i++)
+			{
+				if(letters2 == "a")
+				{
+					System.out.println("a".substring());
+				}
+				if(letters2 == "e")
+				{
+					System.out.println("");
+				}
+				if(letters2 == "i")
+				{
+					System.out.println("");
+				}
+				if(letters2 == "o")
+				{
+					System.out.println("");
+				}
+				if(letters2 == "u")
+				{
+					System.out.println("");
+				}
+				else 
+				{
+					System.out.println("Your name has no vowels");
+				}
+			}							
+			return letters2;	
 				
 			}
 			
